c308d7b300dbe5a43d77440a3c60c6a2bfa055da
Mark Longair <mhl@pobox.com>
2017-06-19 13:08:10 +0100

    Initial version of the scraper

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
+This is a scraper that runs on [Morph](https://morph.io). To get
+started [see the documentation](https://morph.io/documentation)
+
+This scrapes the results of the French legislative elections
+from 2017 from here: http://elections.interieur.gouv.fr/legislatives-2017/index.html
--- scraper.py
+++ scraper.py
@@ -1,24 +1,111 @@
+# -*- coding: utf-8 -*-
+
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import json
+import re
+from urlparse import urljoin
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+import scraperwiki
+import lxml.html
+
+
+def tidy_element_text(element):
+    s = element.text_content()
+    return re.sub(r'\s+', ' ', s.strip())
+
+
+def parse_table(table, department_id, cir_number, tour):
+    rows = table.cssselect('tr')
+    fields = [tidy_element_text(th) for th in rows[0].cssselect('th')]
+    results = []
+    for row in rows[1:]:
+        d = dict(zip(fields, [tidy_element_text(td) for td in row.cssselect('td')]))
+        d['area_id'] = 'ref:{0}-{1:02d}'.format(department_id, int(cir_number))
+        d['dep_id'] = department_id
+        d['cir_number'] = cir_number
+        d['Tour'] = tour
+        results.append(d)
+    return results
+
+
+def scrape_cir(cir_url, department_id, cir_number):
+    results = []
+    cir_html = scraperwiki.scrape(cir_url)
+    root = lxml.html.fromstring(cir_html)
+    first_round_table = None
+    second_round_table = None
+    for h3 in root.cssselect('h3'):
+        tidied = tidy_element_text(h3)
+        if tidied == u'Résultats de la circonscription au 2d tour':
+            second_round_table = h3.getnext()
+        elif tidied == u'Rappel des résultats de la circonscription au 1er tour':
+            first_round_table = h3.getnext()
+        # Some circonscriptions were decided on the first round,
+        # though,
+        # e.g. http://elections.interieur.gouv.fr/legislatives-2017/056/05604.html
+        elif tidied == u'résultats de la circonscription au 1er tour':
+            first_round_table = h3.getnext()
+
+    if first_round_table is None and second_round_table is None:
+        raise Exception, "No results found in: {0}".format(cir_url)
+
+    for tour, table in (('1', first_round_table), ('2', second_round_table)):
+        if table is None:
+            continue
+        data = parse_table(table, department_id, cir_number, tour)
+        results += data
+
+    # Make sure that exactly one person has elected set to 'Oui':
+    winners_found = 0
+    for r in results:
+        elected = r['Elu(e)'].strip()
+        if elected == 'Oui':
+            winners_found += 1
+
+    if winners_found != 1:
+        print "{0} winners found in:".format(winners_found)
+        print json.dumps(data, indent=2, sort_keys=True)
+        raise Exception("Unexpected number of winners found in {0}".format(cir_url))
+
+    return results
+
+
+def scrape_department(department_url, department_id):
+    results = []
+    department_html = scraperwiki.scrape(department_url)
+    root = lxml.html.fromstring(department_html)
+    titles = root.xpath(u"//*[contains(text(), 'Circonscriptions législatives du département')]")
+    assert len(titles) == 1
+    index_div = titles[0].getparent()
+    for a in index_div.cssselect('a'):
+        link_text = tidy_element_text(a)
+        cir_number = re.search(r'^(\d+)', link_text).group(1)
+        cir_rel_url = a.get('href')
+        cir_url = urljoin(department_url, cir_rel_url)
+        results += scrape_cir(cir_url, department_id, cir_number)
+    return results
+
+
+def scrape_country(country_url):
+    country_html = scraperwiki.scrape(country_url)
+    root = lxml.html.fromstring(country_html)
+    department_options = root.cssselect('select#listeDpt option')
+
+    results = []
+    for i, option in enumerate(department_options):
+        dep_rel_url = option.get('value')
+        if dep_rel_url == '#':
+            continue
+        department_id = re.search('^(\d+)', dep_rel_url).group(1)
+        print dep_rel_url, department_id
+        department_url = urljoin(country_url, dep_rel_url)
+        results += scrape_department(department_url, department_id)
+    return results
+
+data = scrape_country('http://elections.interieur.gouv.fr/legislatives-2017/')
+
+scraperwiki.sqlite.save(
+    unique_keys=['area_id', 'Liste des candidats', 'Nuances'],
+    data=data)
